e15c65346af20daaef240d19d8aefffb84db9705
alemba-21 <alembakiragu@gmail.com>
2022-10-03 13:42:13 +0300

    commit tree 357cc7fee56845c88b310154c749753be8671e7b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
-
-  def index
-    items = Item.all
-    render json: items, include: :user
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+  
+    def index
+      if params[:user_id]
+        user = User.find(params[:user_id])
+        items = user.items
+      else
+        items = Item.all 
+        return render json: items, include: :user
+      end
+      render json: items, except: [:created_at, :updated_at]
+    end
+  
+    def show
+      item = Item.find(params[:id])
+      render json: item, except: [:created_at, :updated_at]
+    end
+  
+    def create
+      if params[:user_id]
+        user = User.find(params[:user_id])
+        item = user.items.create(item_params)
+      else
+        item = Item.create(item_params)
+        render json: item, status: :created
+      end
+      render json: item, status: :created
+    end
+  
+    private
+  
+    def item_params
+      params.permit(:name, :description, :price)
+    end
+  
+    def render_not_found_response
+      render json: {error: "user is not found"}, status: :not_found
+    end
+  
   end
-
-end
